60052e0703268c5dca46a433342c72b356c1b031
ctvanzandt42 <ctvanzandt42@gmail.com>
2017-09-19 14:26:26 -0500

    enum finished

--- src/Month.java
+++ src/Month.java
@@ -1,2 +1,24 @@
 public enum Month {
+    JANUARY("January"),
+    FEBRUARY("February"),
+    MARCH("March"),
+    APRIL("April"),
+    MAY("May"),
+    JUNE("June"),
+    JULY("July"),
+    AUGUST("August"),
+    SEPTEMBER("September"),
+    OCTOBER("October"),
+    NOVEMBER("November"),
+    DECEMBER("December");
+
+    private String englishName;
+
+    Month(String englishName) {
+        this.englishName = englishName;
+    }
+
+    public String getEnglishName() {
+        return englishName;
+    }
 }
